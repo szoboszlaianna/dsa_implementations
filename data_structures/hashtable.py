7361179a99b3da1e9785cb6d51445049ad26f322
Anna Szoboszlai <annaszoboszlai@Annas-Air.lan>
2024-06-19 21:40:27 +0200

    add get, tests and pipeline to test

--- data_structures/hashtable.py
+++ data_structures/hashtable.py
@@ -9,18 +9,55 @@ class HashTable:
         self.data = [None] * self.size
 
     def __len__(self):
+        """
+        Return the length of the HashTable.
+
+        Returns:
+            int: The size of the HashTable.
+        """
         return self.size
 
     def _hash(self, key):
+        """
+        Compute the hash value for the given key using the modulo operation.
+        
+        Parameters:
+            key: The key to compute the hash value for.
+        
+        Return:
+            The hash value of the key.
+        """
         return hash(key) % self.size
 
     def __setitem__(self, key, value):
+        """
+        Set the value at a specific key in the hash table.
+
+        Parameters:
+            key: The key to set the value at.
+            value: The value to set.
+
+        Return:
+            None
+        """
         index = self._hash(key)
         if self.data[index] is None:
             self.data[index] = []
         self.data[index].append([key, value])
 
     def __getitem__(self, key):
+        """
+        Retrieve the value associated with the given key from the hash table.
+
+        Parameters:
+            key: The key to retrieve the value for.
+
+        Return:
+            The value associated with the key.
+
+        Raises:
+            KeyError: If the key is not found in the hash table.
+        """
         index = self._hash(key)
         if self.data[index] is not None:
             for k, v in self.data[index]:
@@ -29,11 +66,41 @@ class HashTable:
         raise KeyError
 
     def __delitem__(self, key):
+        """
+        Delete the key-value pair associated with the given key from the hash table.
+
+        Parameters:
+            key: The key to delete from the hash table.
+
+        Return:
+            None
+
+        Raises:
+            KeyError: If the key is not found in the hash table.
+        """
         index = self._hash(key)
         if self.data[index] is not None:
-            print("hello")
             for k, v in self.data[index]:
                 if k == key:
                     self.data[index].remove([k, v])
                     return
         raise KeyError
+
+    def get(self, key, default=None):
+        """
+        Get the value associated with the given key.
+
+        Parameters:
+            key (Hashable): The key to retrieve the value for.
+            default (Any, optional): The default value to return if the key is not found. Defaults to None.
+
+        Returns:
+            Any: The value associated with the key.
+
+        Raises:
+            KeyError: If the key is not found in the hash table.
+        """
+        try:
+            return self[key]
+        except KeyError:
+            return default
--- data_structures/hashtable_test.py
+++ data_structures/hashtable_test.py
@@ -3,19 +3,31 @@ import pytest
 
 
 def test_should_create_hashtable():
+    """
+    Test case to verify if a HashTable object can be created with a specified size.
+    """
     assert HashTable(size=100) is not None
 
 
 def test_should_report_size():
+    """
+    Test case to verify if the size of the HashTable object can be retrieved.
+    """
     assert len(HashTable(size=100)) == 100
 
 
 def test_should_create_empty_value_in_hashtable():
+    """
+    Test case to verify if an empty HashTable object can be created with a specified size.
+    """
     expected = [None, None, None]
     assert HashTable(size=3).data == expected
 
 
 def test_should_insert_key_value_pairs():
+    """
+    Test case to verify if a key-value pair can be inserted into the HashTable object.
+    """
     hash_table = HashTable(size=100)
 
     hash_table["hello"] = "world"
@@ -25,12 +37,18 @@ def test_should_insert_key_value_pairs():
 
 
 def test_should_raise_key_error_on_empty_hashtable():
+    """
+    Test case to verify if a KeyError is raised when trying to retrieve a key from an empty HashTable object.
+    """
     hash_table = HashTable(size=100)
     with pytest.raises(KeyError):
         hash_table["hello"]
 
 
 def test_should_raise_key_error_on_missing_key():
+    """
+    Test case to verify if a KeyError is raised when trying to retrieve a missing key from a HashTable object.
+    """
     hash_table = HashTable(size=100)
     hash_table["hello"] = "world"
     with pytest.raises(KeyError):
@@ -38,12 +56,18 @@ def test_should_raise_key_error_on_missing_key():
 
 
 def test_should_get_value_by_key():
+    """
+    Test case to verify if the value associated with a key can be retrieved from a HashTable object.
+    """
     hash_table = HashTable(size=100)
     hash_table["hello"] = "world"
     assert hash_table["hello"] == "world"
 
 
 def test_remove_key_value_pair():
+    """
+    Test case to verify if a key-value pair can be removed from a HashTable object.
+    """
     hash_table = HashTable(size=100)
     hash_table["hello"] = "world"
     assert hash_table["hello"] == "world"
@@ -51,6 +75,9 @@ def test_remove_key_value_pair():
 
 
 def test_remove_key_error():
+    """
+    Test case to verify if a KeyError is raised when trying to remove a missing key from a HashTable object.
+    """
     hash_table = HashTable(size=100)
     hash_table["hello"] = "world"
     with pytest.raises(KeyError):
@@ -58,6 +85,41 @@ def test_remove_key_error():
 
 
 def test_remove_on_empty_hashtable():
+    """
+    Test case to verify if a KeyError is raised when trying to remove a key from an empty HashTable object.
+    """
     hash_table = HashTable(size=100)
     with pytest.raises(KeyError):
         del hash_table["hello"]
+
+
+def test_get_missing_key_with_default():
+    """
+    Test case to verify if the default value is returned when trying to retrieve a missing key from a HashTable object.
+    """
+    hash_table = HashTable(size=100)
+    value = hash_table.get("not_found", "default_value")
+    assert value == "default_value"
+
+
+def test_get_existing_key():
+    """
+    Test case to verify if the value associated with a key can be retrieved from a HashTable object.
+    """
+    hash_table = HashTable(size=100)
+    hash_table["hello"] = "world"
+    value = hash_table.get("hello")
+    assert value == "world"
+
+
+def test_should_handle_collisions():
+    """
+    Test case to verify if the HashTable handles collisions correctly.
+    """
+    hash_table = HashTable(size=2)
+
+    hash_table["hello"] = "world"
+    hash_table["friend"] = "good"
+
+    assert hash_table["hello"] == "world"
+    assert hash_table["friend"] == "good"
